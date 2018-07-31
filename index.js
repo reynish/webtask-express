acb7d6772631a684a4c1443543a60eeb94a66340
Alex Reynish <reynish@gmail.com>
2018-07-31 10:09:57 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as Webtask from "webtask-tools";
+// import * as express from "express";
+// import * as bodyParser from "body-parser";
+// import * as Webtask from "webtask-tools";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const Webtask = require('webtask-tools');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Hello ' + req.param('name'));
+});
 
 module.exports = Webtask.fromExpress(app);
