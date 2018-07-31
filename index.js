10ccf91d9da770b679aa6900d4b5de1cf9c31228
japr <alex.reynish@cambridgeconsultants.com>
2018-07-31 12:44:02 +0100

    Added pug

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 // import * as bodyParser from "body-parser";
 // import * as Webtask from "webtask-tools";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const Webtask = require('webtask-tools');
+const express = require("express");
+const bodyParser = require("body-parser");
+const pug = require("pug");
+const Webtask = require("webtask-tools");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) =>
+    res.send(
+        pug.renderFile("template.pug", {
+            name: "Timothy"
+        })
+    )
+);
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('Hello ' + req.param('name'));
+app.post("/", (req, res) => {
+    res.send("Hello " + req.param("name"));
 });
 
 module.exports = Webtask.fromExpress(app);
