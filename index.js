447e2b0353bec179503b027d0a3353e706ba169c
Alex Reynish <reynish@gmail.com>
2018-08-02 10:01:37 +0100

    Updating index.js

--- index.js
+++ index.js
@@ -3,25 +3,33 @@
 // import * as pug from "pug";
 // import * as Webtask from "webtask-tools";
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const pug = require("pug");
-const Webtask = require("webtask-tools");
+const express = require('express');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const Webtask = require('webtask-tools');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) =>
-    res.send(
-        pug.render("p Hello #{name}", {
-            name: req.param("name")
-        })
-    )
+var index = +`
+html
+    head
+        title Hello World
+    body
+        p Hello #{name}
+`;
+
+app.get('/', (req, res) =>
+  res.send(
+    pug.render('p Hello #{name}', {
+      name: req.param('name'),
+    })
+  )
 );
 
-app.post("/", (req, res) => {
-    res.send("Hello " + req.param("name"));
+app.post('/', (req, res) => {
+  res.send('Hello ' + req.param('name'));
 });
 
 module.exports = Webtask.fromExpress(app);
